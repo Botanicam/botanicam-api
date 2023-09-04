861dfaa3c6ccf37618d76e7ab596f250602a373e
Ifaz Ahmed <gdneop@gmail.com>
2023-09-04 05:21:44 +0100

    Revert "setup does contribute"
    This reverts commit cb7b66486d1f824e29806542bc746c11c1caa09a.

--- models/user.mjs
+++ models/user.mjs
@@ -31,14 +31,7 @@ const UserSchema = new Schema({
         validate: [isPassword, "Please enter a valid password"],
         minlength: [6, "Password must be at least 6 characters long"],
         select: false,
-    },
-    doesContribute: {
-        type: Boolean,
-        required: false,
-        default: false,
-        
     }
-
 })
 
 // Hash password before saving to database
